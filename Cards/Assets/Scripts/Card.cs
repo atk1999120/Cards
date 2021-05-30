36422b0f8e861f4778636599364c52bac53beb97
atk1999120 <atk1999120@gmail.com>
2021-05-30 13:14:53 +0900

    ポーカーのプログラミングを作りました

---
+++ Cards/Assets/Scripts/Card.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card {
+
+    public Suit CardSuit;
+    public int CardNumber;
+
+    public Card(Suit suit, int cardNumber){
+        this.CardSuit = suit;
+        this.CardNumber = cardNumber;
+    }
+   
+    /// <summary>
+    /// クラブ、ダイアなどの役
+    /// </summary>
+    public enum Suit
+    {
+        Invalid = -1,
+        Club,
+        Dia,
+        Heart,
+        Spade,
+        Max
+    };
+
+    public static Suit CardSuitJudge(int cardNum)
+    {
+        int suitNumber = 0;
+        // ccardNumを割ってあまりを除去した数字をenum型のSuitniキャストして返します
+        for (int i = 0; i < (int)Suit.Max; i++)
+        {
+            if (cardNum / 13 == i)
+            {
+                suitNumber = i;
+            }
+        }
+        return (Suit)suitNumber;
+    }
+
+
+    public static int CardNumberJudge(int cardNum)
+    {
+        int cardNumber = 0;
+        //cardNumを割ったあまりを返します
+        for (int i = 0; i < 13; i++)
+        {
+            if (cardNum % 13 == i)
+            {
+                cardNumber = i + 1;
+            }
+        }
+        return cardNumber;
+    } 
+}
